8aa1f74e65cb0ab9c3b2dd5d64bd33d6982c65ae
Jin Kim <jink8036@gmail.com>
2021-08-17 10:42:21 -0700

    Add routing and skeleton modules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import "./App.css";
+import React from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies.jsx";
+import MovieForm from "./components/movieForm";
+import Customers from "./components/customers.jsx";
+import Rentals from "./components/rentals.jsx";
+import NotFound from "./components/notFound.jsx";
+import NavBar from "./components/navBar.jsx";
+import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm}></Route>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
